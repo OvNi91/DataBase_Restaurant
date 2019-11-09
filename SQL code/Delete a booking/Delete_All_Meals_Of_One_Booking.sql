de6258269d6be43fdc0a2156c97f7e383de7bf85
MedRoy <mederic.royer@efrei.net>
2019-11-09 17:17:08 +0000

    Ajout du dossier contenant des fonctionalités pour supprimer des informations, pas fonctionnel a priori mais le code semble tout de meme bon

---
+++ SQL code/Delete a booking/Delete_All_Meals_Of_One_Booking.sql
@@ -0,0 +1,5 @@
+/* DELETE ALL THE MEALS OF A BOOKING */
+
+DELETE FROM meal
+WHERE booking_id_fk = '';
+
---
+++ SQL code/Delete a booking/Delete_One_Meal.sql
@@ -0,0 +1,4 @@
+/* DELETE A MEAL  / CANCEL  (ONLY ONE MEAL) */
+
+DELETE FROM meal
+WHERE meal_id_pk = '37';
